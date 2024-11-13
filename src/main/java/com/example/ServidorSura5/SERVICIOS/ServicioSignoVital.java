9cdccf66961bc32ef6a1b78933123763ac86c2f7
Nicolle <nicollexvargas@gmail.com>
2024-11-13 16:11:03 -0500

    Se crean controladores y servicios

---
+++ src/main/java/com/example/ServidorSura5/SERVICIOS/ServicioSignoVital.java
@@ -0,0 +1,21 @@
+package com.example.ServidorSura5.SERVICIOS;
+
+import com.example.ServidorSura5.MODELOS.Medico;
+import com.example.ServidorSura5.MODELOS.SignoVital;
+import com.example.ServidorSura5.REPOSITORIOS.IRepositorioMedico;
+import com.example.ServidorSura5.REPOSITORIOS.IRepositorioSignoVital;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ServicioSignoVital {
+    @Autowired
+    IRepositorioSignoVital iRepositorioSignoVital;
+    public SignoVital guardarSignoVital(SignoVital datosSignoVital) throws Exception{
+        try{
+            return iRepositorioSignoVital.save(datosSignoVital);
+        }catch (Exception error){
+            throw new Exception(error.getMessage());
+        }
+    }
+}
